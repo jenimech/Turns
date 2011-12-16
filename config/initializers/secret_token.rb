8a1c1e207b979b15ea514326099511c3a7c290e3
Maria Eugenia Chacon <jenimech2@gmail.com>
2011-12-16 20:46:32 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SysTurns::Application.config.secret_token = '43bb42d72a4a81a4b38ba50e0f9581ae7ab9c206d958f49cab15b79eabdcb9a05fc495b36759906294f9cc9c713bfec8249585f281daf45fbf667428dbebbf4a'
