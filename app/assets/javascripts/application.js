14a03ea8be741cd4bab767c46e48695a3af790c9
Maria Eugenia Chacon <jenimech2@gmail.com>
2012-01-11 14:41:26 -0300

    move js and added files to precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,47 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= requier dashboard
+//= require fullcalendar.min
+//= require gcal.js
+//= require jquery-ui-1.8.16.custom.min
+//= require jquery.popupWindow.js
 //= require_tree .
 
-$(document).ready(function() {
-     gd = new Date();
-     y  = gd.getFullYear();
-     m  = gd.getMonth();
-     d  = gd.getDate();
-    $('#calendar').fullCalendar({
-      weekends: false,
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      editable: true,
-      dayClick: function(date, allDay, jsEvent, view) {
-        d = date.getDate();
-        m = date.getMonth();
-        Y = date.getFullYear();
-        H = date.getHours();
-        M = date.getMinutes();
-        day   = new Date(Y, m, d)
-        hours = new Date(Y, m, d, H, M)
-        $('#turn_day').val(day);
-        $('#turn_hour').val(hours);
-        $('#add_turn').dialog({title: 'Add a New Turn', close:'close'});
-      },
-      events: loadEvents()
-    });
-
-});
-
-function loadEvents(){
-
-   var events = []
-   $('.event').each(function(index, e){
-     turn = $(e)
-     events[index] = {
-       title: turn.attr('data_client'), 
-       start: turn.attr('data_date_start'),
-       end: turn.attr('data_date_end')
-     }
-   })
-   return events;
-}
